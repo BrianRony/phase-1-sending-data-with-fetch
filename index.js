1005db35a83d1fcf0bd44da09f14bb5d5246ba0b
Brian Ngetich <kipronobrian97@gmail.com>
2024-04-13 14:47:02 +0300

    Fetch added

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+function submitData (name,email){
+    return fetch(`http://localhost:3000/users` ,{
+        method: "POST",
+        headers: {
+            "content-type":"application/json",
+            "accept":"application/json"
+        },
+        body:JSON.stringify({
+            name,
+            email})
+    })
+    .then(res => res.json())
+    .then((data) => {
+        document.body.innerHTML = data["id"]
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
